5f1a029b8be0e8e1e697415e363b373239e16076
maldonadoj4atwit <maldonadoj4@wit.edu>
2025-11-06 00:35:39 -0500

    Completed partb.c

--- partb.c
+++ partb.c
@@ -1,8 +1,32 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
-    return 0;
+    if ( argc == 1 ) {
+        printf( "ERROR: No arguments\n" ) ;
+        return 1 ;
+    }
+
+    int count = argc - 1 ;
+
+    if ( count == 1 ) {
+        printf( "%s\n", argv[ 1 ] ) ;
+        return 0 ;
+    }
+
+    if ( count % 2 == 0 ) {
+        int mid1 = count / 2 ;
+        int mid2 = mid1 + 1 ;
+        printf( "%s %s\n", argv[ mid1 ], argv[ mid2 ] ) ;
+    } 
+    
+    else {
+        int mid = ( count / 2 ) + 1 ;
+        printf( "%s\n", argv[ mid ] ) ;
+    }
+
+    return 0 ;
 }
