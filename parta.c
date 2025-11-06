c58a2a835b290418ef37be8d65cdeb28b9a5a360
maldonadoj4atwit <maldonadoj4atwit>
2025-11-06 10:07:08 -0500

    Completed part A

--- parta.c
+++ parta.c
@@ -1,8 +1,27 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
-    return 0;
+    if ( argc == 1 ) {
+        printf( "ERROR: No arguments\n" ) ;
+        return 1 ;
+    }
+
+    for ( int i = 1 ; i < argc ; i++ ) {
+        const char* arg = argv[ i ] ;
+        for ( int j = 0 ; argv[ i ][ j ] != '\0' ; j++ ) {
+            printf( "%c", toupper( argv[ i ][ j ] ) ) ;
+        }
+        
+        if ( i < argc - 1 ) {
+            printf( "," ) ;
+        }
+    }
+
+    printf( "\n" ) ;
+    return 0 ;
 }
